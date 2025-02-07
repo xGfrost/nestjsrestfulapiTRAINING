234e8f24b892bee7cdead1f981a3684b8f7f1946
xGfrost <fadelakbar528@gmail.com>
2025-02-07 18:44:09 +0700

    add api remove contact by id

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { ContactService } from './contact.service';
 import { Auth } from "src/common/auth.decorator";
 import { User } from "@prisma/client";
@@ -46,6 +46,18 @@ export class ContactController{
         const result = await this.contactService.update(user, request); 
         return {
             data: result,
+        }
+    }
+
+    @Delete('/:contactId')
+    @HttpCode(200)
+    async remove(
+        @Auth() user: User,
+        @Param('contactId', ParseIntPipe) contactId: number,
+    ): Promise<WebResponse<boolean>>{
+        await this.contactService.remove(user, contactId); 
+        return {
+            data: true,
         }
     }
 }
